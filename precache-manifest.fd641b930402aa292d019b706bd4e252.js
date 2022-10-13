38208051e0913035b67d3c1f3edb48b04b12f1d4
Oscar Ruiz Antonio <86693936+Drancer55@users.noreply.github.com>
2022-10-13 12:48:40 -0500

    Updates

---
+++ precache-manifest.fd641b930402aa292d019b706bd4e252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "640e4f9d32d1b6f43f4a",
+    "url": "/JobApp/static/css/main.2b042889.chunk.css"
+  },
+  {
+    "revision": "640e4f9d32d1b6f43f4a",
+    "url": "/JobApp/static/js/main.640e4f9d.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/JobApp/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/JobApp/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "763728b2a747cf694d88",
+    "url": "/JobApp/static/js/runtime~main.763728b2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/JobApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9d36ff40e5a964754774077687fe9dd4",
+    "url": "/JobApp/index.html"
+  }
+];
